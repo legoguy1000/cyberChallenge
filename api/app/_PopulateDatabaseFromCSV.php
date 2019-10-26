cffdd4958285a69962738bb480ff373abb888c6e
Alex Resnick <adr8292@gmail.com>
2019-10-26 00:46:01 -0500

    create file to import questions from CSV

---
+++ api/app/_PopulateDatabaseFromCSV.php
@@ -0,0 +1,39 @@
+<?php
+require_once(__DIR__.'/includes.php');
+
+
+$file = $argv[1];
+if(!is_file($file)) {
+  die('Not a file');
+}
+if(substr($file, -4)  != '.csv') {
+  die('Not a CSV file');
+}
+if (($handle = fopen($file, "r")) !== FALSE) {
+  $headers = fgetcsv($handle, 1000, ",");
+  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    $row = array_combine($headers,$data);
+    $cat = CyberChallenge\Category::where('name', $row['category'])->first();
+    if(is_null($cat)) {
+      $cat = new CyberChallenge\Category(['name'=>$row['category']]);
+      $cat->save();
+    }
+    $quest = new CyberChallenge\Question([
+      'hint_1'=>$row['hint_1'],
+      'hint_2'=>$row['hint_2'],
+      'hint_3'=>$row['hint_3'],
+    ]);
+    $cat->questions()->save($quest);
+    $answers = array(
+      new CyberChallenge\Answer(['answer'=>$row['answer_1']]), //Correct Answer
+      new CyberChallenge\Answer(['answer'=>$row['answer_2']]),
+      new CyberChallenge\Answer(['answer'=>$row['answer_3']]),
+      new CyberChallenge\Answer(['answer'=>$row['answer_4']]),
+    );
+    $quest->answers()->saveMany($answers);
+    $quest->correct_answer_id = $answers[0]->answer_id;
+    $quest->save();
+  }
+  fclose($handle);
+}
+?>
--- api/app/initialInstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(__DIR__.'/includes.php');
-
-
-shell_exec("composer install");
-shell_exec("composer dump-autoload");
-
-?>
